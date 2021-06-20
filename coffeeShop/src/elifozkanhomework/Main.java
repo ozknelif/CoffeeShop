8b9f92a3c1692e85e0b2f9152d4b6cfa92e7f816
Elif <ozknelif@gmail.com>
2021-06-20 15:06:37 +0300

    Gereksinim 1 için commitlenmiştir.
    Main içerisinde SiparisSistemi sınıfından bir nesne üretilmiştir.
    Sistem sürekli çalışacak hale getirilmiştir.

--- coffeeShop/src/elifozkanhomework/Main.java
+++ coffeeShop/src/elifozkanhomework/Main.java
@@ -3,11 +3,16 @@ package elifozkanhomework;
 
 public class Main {
 	
-	
 	public static void main(String[] args) {
 		
+		SiparisSistemi sistem = new SiparisSistemi();
 		
-
+		while(true) 
+		{
+			sistem.menuGoster();
+			sistem.siparisAl();
+		}
+		
 	}
 
 }
